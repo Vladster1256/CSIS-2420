9840d86370d3a1cf9a95476b3c9bebea0eeccc62
Vladlen <vcmariorocks@gmail.com>
2016-03-08 10:37:25 -0700

    added code

--- src/comparator/RectangleApp.java
+++ src/comparator/RectangleApp.java
@@ -7,7 +7,7 @@ public class RectangleApp
 
 	public static void main(String[] args)
 	{
-		Rectangle[] rectangles = {new Rectangle(2,1), new Rectangle (4,3), new Rectangle (6,5)};
+		Rectangle[] rectangles = {new Rectangle(2,1), new Rectangle (4,3), new Rectangle (6,5), new Rectangle(4,2)};
 		System.out.println(Arrays.toString(rectangles));
 		Arrays.sort(rectangles);
 		System.out.println(Arrays.toString(rectangles));
